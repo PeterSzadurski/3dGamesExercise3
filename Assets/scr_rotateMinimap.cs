1611fb0a7e40f758305776b6050dff10a4ed4087
Peter Szadurski <44846197+PeterSzadurski@users.noreply.github.com>
2020-10-03 22:31:28 -0400

    Minimap and Radar

---
+++ Assets/scr_rotateMinimap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_rotateMinimap : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _MapUi;
+
+    private Transform _Player;
+    void Start()
+    {
+        _Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 compassAngle = new Vector3();
+        compassAngle.z = _Player.transform.eulerAngles.y;
+        _MapUi.transform.eulerAngles = compassAngle;
+    }
+}
